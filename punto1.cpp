b85c841b14b8d0261597d57453d03c9d7b206b9b
RafaelOchoaVillegas <42081580+RafaelOchoaVillegas@users.noreply.github.com>
2018-08-17 13:53:49 -0500

    Add files via upload

---
+++ punto1.cpp
@@ -0,0 +1,12 @@
+// Example program
+#include <iostream>
+using namespace std;
+
+int main()
+{
+float numero1; 
+int numero3 = numero1;
+
+  cout << "entre numero con decimales: ";  cin >> numero1;
+   cout <<"el nuevo numero en formato entero es: "<<numero1<<endl;
+}  
